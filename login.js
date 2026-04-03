d432e74e128d046aaf813eca85ff104f5db309b6
Symon <nirmalbdrbk@gmail.com>
2026-04-03 08:46:45 +0545

    js of login-page

---
+++ login.js
@@ -0,0 +1,19 @@
+document.writeln("Hello World!");
+window.alert("Welcome to my website");
+
+let name = prompt("What is your name?");    
+let age = prompt("What is your age?");
+document.writeln("Hello " + name + "! You are " + age + " years old."); 
+
+let name1 = "TECHSPAIRE";
+let year = 2029;
+document.writeln(year);
+
+let a = 10;
+let  b = 20;
+let sum = a + b;
+
+
+a = [1,2,3,4,5];
+document.writeln("array:\n" + a[2]);
+
